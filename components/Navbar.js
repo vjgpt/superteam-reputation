7b3ff923195adcc69785b3fc2aa978c51abace11
vjgpt <vjgupta57@gmail.com>
2021-11-11 15:46:20 +0530

    Fixed bugs for script tag and class usage

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,33 +1,32 @@
 import Link from 'next/link'
-import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light">
-        <Link href="https:/superteam.fun"><a class="navbar-brand" >Superteam</a></Link>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light">
+        <Link href="https:/superteam.fun"><a className="navbar-brand" >Superteam</a></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-3 mt-lg-0">
-            <li class="nav-item active">
-            <Link href="/"><a class="nav-link" >Leaderboard</a></Link>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav mr-auto mt-3 mt-lg-0">
+            <li className="nav-item active">
+            <Link href="/"><a className="nav-link" >Leaderboard</a></Link>
             </li>
-            <li class="nav-item">
-            <Link href="/dashboard"><a class="nav-link" >Dashboard</a></Link>
+            <li className="nav-item">
+            <Link href="/dashboard"><a className="nav-link" >Dashboard</a></Link>
             </li>
-            <li class="nav-item">
-            <Link href="/bounty"><a class="nav-link" >Bounty Board</a></Link>
+            <li className="nav-item">
+            <Link href="/bounty"><a className="nav-link" >Bounty Board</a></Link>
             </li>
-            <li class="nav-item dropdown ">
-                <a class="nav-link dropdown-toggle"  id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <li className="nav-item dropdown ">
+                <a className="nav-link dropdown-toggle"  id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">
                 Projects
                 </a>
-                <div class="dropdown-menu m-0" aria-labelledby="navbarDropdown">
-                <Link href="/project/7c10df77534f43399203609b0d2ae5c2"><a class="dropdown-item" >Reputation System</a></Link>
-                <Link href="/project/d2c8414f2b014c32840f9aa80bce6d08"><a class="dropdown-item" >Mission 5K</a></Link>
-                <Link href="/project/d2c8414f2b014c32840f9aa80bce6d08"><a class="dropdown-item" >Web2 to Web3 Education</a></Link>
+                <div className="dropdown-menu m-0" aria-labelledby="navbarDropdown">
+                <Link href="/project/7c10df77534f43399203609b0d2ae5c2"><a className="dropdown-item" >Reputation System</a></Link>
+                <Link href="/project/d2c8414f2b014c32840f9aa80bce6d08"><a className="dropdown-item" >Mission 5K</a></Link>
+                <Link href="/project/d2c8414f2b014c32840f9aa80bce6d08"><a className="dropdown-item" >Web2 to Web3 Education</a></Link>
                 </div>
             </li>
             </ul>
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import '../styles/globals.css'
 import Layout from '../components/Layout'
 import '../styles/Navbar.module.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -10,11 +11,11 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Reputation Leaderboard</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+        <Script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></Script>
       <Layout>
           <Component {...pageProps} />
       </Layout>
